90a7fdfa0486608878350ea6b644a9a03a8c899b
XReVo <thefuckermail@mail.ru>
2016-10-06 10:02:21 +0000

    IN integer
    سكربت فحص مضاعفات العدد 2
    برنامج فحص مضاعافت العدد 2
    #XReVo

---
+++ IN.py
@@ -0,0 +1,21 @@
+import os
+os.system('color a')
+open('sebiria.html','w')
+def Admin () :
+    print ' Do not WRITE a "String" ' 
+    s=int(raw_input('Type The number You want to Check it  :\n------>: ' ))
+    a= s /2 
+    b=a*2 
+    c=s-1  
+    d = c /2
+    if  b==s :
+        print '--->The Number ',s, ' / 2 Because  ',a,'*2=',s
+    else :
+        print " ---> The Number ",s, "dont \ Because " , s , '=' , '1 + ' , d , '*2'
+while True :
+    Admin()
+
+    
+
+    
+
